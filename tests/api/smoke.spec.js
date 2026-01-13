9c513019b41194dba53ee1b0e3f3349548ab43dc
zjtheilen <zjtheilen@gmail.com>
2026-01-13 12:28:37 -0800

    expand smoke test, proof of concept, accessing and displaying data from returned object

--- tests/api/smoke.spec.js
+++ tests/api/smoke.spec.js
@@ -1,17 +1,60 @@
 const { test, expect, request } = require('@playwright/test');
 
-test('basic GET request to Pokemon API', async () => {
-    const apiContext = await request.newContext();
-    
-    const response = await apiContext.get('https://pokeapi.co/api/v2/pokemon/pikachu');
-    
-    expect(response.status()).toBe(200);
+test.describe('Pokemon API basic tests', () => {
+    let apiContext;
 
-    const data = await response.json();
+    test.beforeAll(async () => {
+        apiContext = await request.newContext();
+    });
 
-    expect(data.name).toBe('pikachu');
+    test.afterAll(async () => {
+        await apiContext.dispose();
+    });
 
-    console.log('Pokemon name:', data.name);
+    test('pikachu test', async () => {    
+        const response = await apiContext.get('https://pokeapi.co/api/v2/pokemon/pikachu');
+        expect(response.status()).toBe(200);
 
-    await apiContext.dispose();
-});
+        const data = await response.json();
+
+        expect(data.name).toBe('pikachu');
+        console.log('Pokemon name:', data.name);
+
+        console.log('\n--- Pikachu Test Complete ---\n');
+    });
+
+    test('squirtle test', async () => {
+        const response = await apiContext.get('https://pokeapi.co/api/v2/pokemon/squirtle');
+        expect(response.status()).toBe(200);
+
+        const data = await response.json();
+
+        const { 
+            name: pokeName, 
+            id: pokeId, 
+            abilities: pokeAbilities, 
+            stats: pokeStats, 
+            types: pokeTypes 
+        } = data;
+
+        console.log("Pokemon name:", pokeName);
+        console.log("Pokemon ID:", pokeId);
+
+        console.log('Pokemon abilities:')
+        for (const ability of pokeAbilities) {
+            console.log('-- ability:', ability.ability.name)
+        }
+
+        console.log('Pokemon stats:')
+        for (const stat of pokeStats) {
+            console.log('-- stat:', stat.stat.name)
+        }
+
+        console.log('Pokemon types:');
+        for (const type of pokeTypes) {
+            console.log('-- type:', type.type.name);
+        }
+
+        console.log('\n--- Squirtle Test Complete ---\n');
+    });
+})
