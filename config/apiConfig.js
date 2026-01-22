cf1cab2b97f4afcd56d3ade69ee531afd82f2b05
zjtheilen <zjtheilen@gmail.com>
2026-01-22 10:24:55 -0800

    add JSONPlaceholder API config and helper

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -1,1 +1,8 @@
-module.exports = { baseUrl: 'https://pokeapi.co/api/v2' };
+module.exports = {
+  pokemonApi: {
+    baseUrl: 'https://pokeapi.co/api/v2'
+  },
+  jsonPlaceholderApi: {
+    baseUrl: 'https://jsonplaceholder.typicode.com'
+  }
+};
