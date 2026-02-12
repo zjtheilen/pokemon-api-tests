17584ccd698e458bb8be8b9f7c260c3344ee2e6f
zjtheilen <zjtheilen@gmail.com>
2026-02-12 10:07:32 -0800

    add colorized summary reporter for playwright v1.1.2

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,14 @@
 const { defineConfig } = require('@playwright/test');
 
-const config = {
-    testDir: './tests',
-    timeout: 30000,
-    workers: 1, // run everything in a single Node process
-    reporter: [['list'], ['html']],
-     use: {
-        extraHTTPHeaders: {
-            'Accept': 'application/json',
-        },
-    },
-};
-
-module.exports = config;
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  reporter: [
+    ['list'],
+    ['html'],
+    ['./color-summary-reporter.js'] // <- make sure path matches where you saved it
+  ],
+  use: {
+    extraHTTPHeaders: { Accept: 'application/json' },
+  },
+});
--- tests/e2e/zzz.summary.spec.js
+++
@@ -1,6 +0,0 @@
-const { test } = require('@playwright/test');
-const summary = require('../../helpers/testSummaryHelper');
-
-test('Print aggregated summary', async () => {
-    summary.printSummary();
-});
